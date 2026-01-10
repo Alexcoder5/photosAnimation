fd05578a62a9e1953a3aab6987dc0a3b11186eef
Alexandr Gutul <gutulalexandr10@gmail.com>
2026-01-10 13:22:10 +0700

    Initial Commit

---
+++ photosAnimation/photosAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  photosAnimationApp.swift
+//  photosAnimation
+//
+//  Created by Alexandr Gutul on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct photosAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
